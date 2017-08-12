4c4ecec6d9dadc6ae9a672485551df6413ccd733
devbywater <egaraudy@gmail.com>
2017-08-12 16:47:02 -0600

    sg-v1 base application dev env works

--- sg-v1/index.js
+++ sg-v1/index.js
@@ -1,12 +1,16 @@
 const path = require('path')
 const express = require('express')
+const logger = require('morgan')
 
 const app = express()
-
 const PORT = process.env.PORT || 3005
 
+app.use(logger('combined'))
 app.use(express.static(path.join(__dirname, 'client/build')))
 
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
 
 app.listen(PORT, function() {
     console.log(`App listening on ${PORT}`)
